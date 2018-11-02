94885799a7103813bea6e61b77fa811ea1d9f49d
gordislava <gordislava@gmail.com>
2018-11-02 13:59:07 +0300

    js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,23 +1,7 @@
-// var headerMenu = document.querySelector(".header__menu");
-// var headerButtonMenu = document.querySelector(".header__button-menu");
 var orderButton = document.querySelector(".promo__button");
 var productItem = document.querySelectorAll(".product-item__button");
 var modalPurchase = document.querySelector(".modal__container");
 var modalOverlay = document.querySelector(".modal");
-
-// headerButtonMenu.classList.remove("header__button-menu--no-js");
-//
-// headerButtonMenu.addEventListener("click", function() {
-//   if (headerButtonMenu.classList.contains("header__button-menu--open")) {
-//     headerButtonMenu.classList.remove("header__button-menu--open");
-//     headerButtonMenu.classList.add("header__button-menu--close");
-//   }
-//
-//   else {
-//     headerButtonMenu.classList.remove("header__button-menu--close");
-//     headerButtonMenu.classList.add("header__button-menu--open");
-//   }
-// });
 
 if (orderButton) {
 orderButton.addEventListener("click", function(evt) {
